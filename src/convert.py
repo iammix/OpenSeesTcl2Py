d55137fe0f3a3235d6b58413225aca74de2d2912
Konstantinos Mixios <k.mixios@gmail.com>
2022-04-27 13:43:39 +0300

    Change version of workflow to 4.5

--- src/convert.py
+++ src/convert.py
@@ -4,17 +4,17 @@ from pathlib import Path
 
 class ConvertTcl2Py():
 
-    def __init__(self, tclFileName:str):
+    def __init__(self, tclFileName: str):
         self.tclFileName = tclFileName
         self.pyLines = []
 
-    def _get_lines(self)->str:
+    def _get_lines(self) -> str:
         with open(self.tclFileName, 'r') as tclFile:
             tclLines = tclFile.readlines()
         tclFile.close()
         return tclLines
-    
-    def _get_node_lines(self)->str:
+
+    def _get_node_lines(self) -> str:
         node_lines = []
         with open(self.tclFileName, 'r') as tclFile:
             tclLines = tclFile.readlines()
@@ -24,11 +24,10 @@ class ConvertTcl2Py():
         tclFile.close()
         return node_lines
 
-
     def tcl2py(self):
         with open(self.tclFilename, 'r') as tclFile:
             tclLines = tclFile.readlines()
-    
+
     def node(self):
         lines = self._get_node_lines()
         node_tag = []
@@ -38,19 +37,25 @@ class ConvertTcl2Py():
         for line in lines:
             line_list = line.split('\t')
             if len(line_list) > 1:
-                node_tag.append(line_list[0])
-                x.append(line_list[1])
-                y.append(line_list[2])
-                z.append(line_list[3])
+                node_tag.append(int(line_list[1]))
+                x.append(float(line_list[2]))
+                y.append(float(line_list[3]))
+                z.append(float(line_list[4]))
             else:
                 pass
-        print(node_tag, x, y, z)
-
-
+        self.node_lines = []
+        for i in range(len(node_tag)):
+            print(z[i])
+            self.node_lines.append(f"ops.node({node_tag[i]}, {x[i]}, {y[i]}, {z[i]})")
+        return self.node_lines
 
 
 if __name__ == "__main__":
     project_path = Path(__file__).absolute().parent
     tclFileName = os.path.join(project_path, "Nodes.tcl")
     convert = ConvertTcl2Py(tclFileName)
-    convert.node()
+    lines = convert.node()
+    with open('python.py', 'w') as pythonFile:
+        for line in lines:
+            pythonFile.write(line + '\n')
+    pythonFile.close()
